9c18f743cc6b2160cd5d1cd43b3ab8e4ebfd8e4c
Karl Movido <karl.movido@gmail.com>
2026-07-03 18:11:17 +0800

    Added visual changes, createBookCard(), displayBooks(), and formatStatus()

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const myLibrary = JSON.parse(localStorage.getItem('books')) || [
-	new Book('Atomic Habits', 'James Clear', 320, 'Done', 320),
-	new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'Reading', 145),
-	new Book('1984', 'George Orwell', 328, 'To Read', 0),
-	new Book('The Psychology of Money', 'Morgan Housel', 256, 'Reading', 78),
-	new Book('Dune', 'Frank Herbert', 688, 'To Read', 0),
-	new Book('The Alchemist', 'Paulo Coelho', 208, 'Done', 208),
+	{
+		id: crypto.randomUUID(),
+		title: 'The Housekeeper and the Professor',
+		author: 'Yoko Ogawa',
+		pages: 180,
+		currentPage: 67,
+		status: 'reading',
+		pinned: true,
+	},
+	{
+		id: crypto.randomUUID(),
+		title: 'The Hobbit',
+		author: 'J.R.R. Tolkien',
+		pages: 295,
+		currentPage: 0,
+		status: 'to-read',
+		pinned: false,
+	},
 ];
 
 function Book(title, author, pages, status, pagesRead) {
@@ -14,6 +26,7 @@ function Book(title, author, pages, status, pagesRead) {
 	this.pages = pages;
 	this.status = status;
 	this.pagesRead = pagesRead;
+	this.pinned = false;
 }
 
 const addBookBtn = document.getElementById('add-book');
@@ -62,3 +75,82 @@ addToShelf.addEventListener('click', () => {
 	addBookToLibrary();
 	console.log(myLibrary);
 });
+
+// Display cards on the shelf
+function displayBooks() {
+	const shelf = document.getElementById('shelf');
+	shelf.innerHTML = '';
+
+	myLibrary.forEach((book) => {
+		const card = createBookCard(book);
+		shelf.appendChild(card);
+	});
+}
+
+// Create card for books
+function createBookCard(book) {
+	const card = document.createElement('article');
+
+	card.classList.add('book-card');
+
+	if (book.pinned) {
+		card.classList.add('pinned');
+	}
+
+	card.classList.add(book.status);
+
+	const progress = Math.round((book.currentPage / book.pages) * 100);
+
+	card.innerHTML = `
+		<div class="pin-border"></div>
+		<div class="card-top">
+			<button class="edit-btn">
+				<i data-lucide="square-pen"></i>
+			</button>
+		</div>
+		<div class="card-body">
+			<h3 class="title">${book.title}</h3>
+			<p class="author"><span>by</span> ${book.author}</p>
+			<div class="progress-wrap">
+				<div class="progress-label">
+					Page
+					<span class="page-editor">
+						<button class="page-display" id="pageDisplay">${book.currentPage}</button>
+						<input type="number" class="current-page-input hidden" id="pageInput" value="${book.currentPage}" min="0" max="${book.pages}" />
+					</span>
+					of ${book.pages} pages
+				</div>
+				<div class="progress-bar"><div class="progress-bar-fill" style="width: ${progress}%"></div></div>
+			</div>
+			<div class="card-actions">
+				<button class="pin-btn">
+					<i data-lucide="bookmark"></i>
+				</button>
+				<button class="done-btn">
+					<i data-lucide="check"></i>
+				</button>
+				<button class="status-btn ${book.status}">${formatStatus(book.status)}</button>
+			</div>
+		</div>
+	`;
+
+	return card;
+}
+
+// Format helper for status button
+function formatStatus(status) {
+	switch (status) {
+		case 'to-read':
+			return 'To read';
+
+		case 'reading':
+			return 'Reading';
+
+		case 'done':
+			return 'Done';
+	}
+}
+
+displayBooks();
+
+lucide.createIcons();
